cc5cb1fcdd2dd702f7592428db01d7fbe6f750f0
makbeard00 <mkjustice00@gmail.com>
2020-08-16 14:57:27 -0400

    Added larger colliders to the spheres which when raycasted will enable a glow effect so player knows when he/she can jump to another pedestal!

---
+++ Assets/Scripts/Glow.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glow : MonoBehaviour
+{
+    [SerializeField] GameObject glowPoint;
+    [SerializeField] float glowTime = 1f;
+
+    bool isGlowing = false;
+    float timer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(glowPoint == null)
+        {
+            Debug.Log("Missing glowPoint on: " + gameObject.name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGlowing)
+        {
+            timer += Time.deltaTime;
+            if(timer >= glowTime)
+            {
+                GlowOff();
+                timer = 0f;
+            }
+        }
+    }
+
+    public void GlowOn()
+    {
+        glowPoint.SetActive(true);
+        isGlowing = true;
+    }
+
+    public void GlowOff()
+    {
+        glowPoint.SetActive(false);
+        isGlowing = false;
+    }
+
+    
+}
--- Assets/Scripts/WarpControls.cs
+++ Assets/Scripts/WarpControls.cs
@@ -7,9 +7,13 @@ using UnityStandardAssets.CrossPlatformInput;
 public class WarpControls : MonoBehaviour
 {
     bool canWarp = false;
+    bool isWarping = false;
+    GameObject nextWarpPoint;
+    GameObject currentWarpPoint;
     Camera camera;
 
     [SerializeField] GameObject firstPedestalMount;
+    [SerializeField] float warpSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,27 +21,71 @@ public class WarpControls : MonoBehaviour
         // move player to assigned pedestal
         gameObject.transform.position = firstPedestalMount.transform.position;
         firstPedestalMount.GetComponent<Renderer>().enabled = false;
+        currentWarpPoint = firstPedestalMount;
 
         camera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        Ray ray = new Ray(transform.position, transform.forward);
+        if (CrossPlatformInputManager.GetButtonDown("Jump") && canWarp)
+        {
+            WarpToMountPoint();
+        }
+
+        if (isWarping)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, nextWarpPoint.transform.position, warpSpeed * Time.deltaTime);
+            if(Vector3.Distance(transform.position, nextWarpPoint.transform.position) <= 0.1f)
+            {
+                isWarping = false;
+            }
+        }
+
+        if(nextWarpPoint != null && !isWarping && canWarp)
+        {
+            nextWarpPoint.GetComponentInParent<Glow>().GlowOn();
+        }
+        else if (isWarping)
+        {
+            nextWarpPoint.GetComponentInParent<Glow>().GlowOff();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        Ray ray = new Ray(transform.position + transform.up, transform.forward);
         RaycastHit hit;
-        Debug.DrawRay(transform.position, transform.forward);
-        if(Physics.Raycast(ray, out hit, 10f))
+        if(Physics.Raycast(ray, out hit))
         {
             if(hit.transform.tag == "Pedestal")
             {
-                print("I'm looking at " + hit.transform.name);
+                canWarp = true;
+                nextWarpPoint = hit.transform.gameObject;
+                
             }
-        }
-        else
+            else
+            {
+                canWarp = false;
+
+            }
+
+        }    
+    }
+
+    public void WarpToMountPoint()
+    {
+        if (canWarp)
         {
-            print("I'm looking at nothing!");
+            print("I'm a warping!");
+            //gameObject.transform.position = warpPointLookingAt.transform.position;
+            isWarping = true;
+            nextWarpPoint.GetComponent<Renderer>().enabled = false;
+            currentWarpPoint.GetComponent<Renderer>().enabled = true;
+            currentWarpPoint = nextWarpPoint;
+            canWarp = false;
         }
     }
-    
 }
